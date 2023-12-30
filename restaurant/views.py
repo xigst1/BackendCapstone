3033970df9a7131932554e4c2bc2d1c172cbde33
xigst1 <xhgeb@hotmail.com>
2023-12-29 19:48:22 -0800

    djoser and tokenAuthentication api (#4)

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,15 +3,24 @@ from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
-from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html', {})
 
+
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
+
 class MenuItemView(generics.ListCreateAPIView):
     
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     
@@ -71,7 +80,7 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     
 
 
